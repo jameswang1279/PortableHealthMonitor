22639a7a5824823386b41df6f420ada42c6eaa07
James Wang <jamesw2320tablet@gmail.com>
2015-07-14 16:30:52 -0700

    MatPlot Temperature Live Graphing
    Not Tested

---
+++ TemperatureLiveGraph.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import serial
+from drawnow import *
+
+temperature = []
+arduinoData = serial.Serial('COM4', 115200)
+plt.ion
+cnt = 0
+
+def makeFig():
+    plt.xlim(0,100)
+    plt.ylim(35,45)
+    plt.title("Body Temperature Log")
+    plt.grid(True)
+    plt.ylabel("Temperature (Celsius) ")
+    plt.plot(temperature, 'ro-',leabel = 'Degrees Celsius')
+    plit.legend(loc = 'upper left')
+
+while True:
+            while(arduinoData.inWaiting() ==0):
+                pass
+            arduinoString = arduinoData.readline()
+            temp = float(dataArray[0])
+            temperature.append(temp)
+            drawnoe(makeFig)
+            plt.pause(0.0001)
+            if(cent > 50):
+                    temp.pop(0)
