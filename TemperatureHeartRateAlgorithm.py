3c98c2a62a7327c99d91233c8a9f608a471f4d3d
James Wang <jamesw2320tablet@gmail.com>
2015-07-31 12:44:32 -0700

    Delete Temp Files

--- TemperatureHeartRateAlgorithm.py
+++ TemperatureHeartRateAlgorithm.py
@@ -6,12 +6,12 @@ tempArray = []
 counter = 0
 
 while counter < 10:
-	arduinoData = serial.Serial('/dev/ttyACM2', 115200)
+	arduinoData = serial.Serial('COM5', 115200)
 	#arduinoData = arduinoData.split(',')
 	tempArray.append(arduinoData)
 	counter += 1
 
-print tempArray
+print (tempArray)
 
 '''
 for i in range(len(tempArray)):
@@ -21,8 +21,8 @@ for j in range(0,len(tempArray),2):
 for k in range(1,len(tempArray),2):
 	dataHeart.append(tempArray[k])
 '''
-print arduinoData
-print dataTemp
-print dataHeart
+print (arduinoData)
+print (dataTemp)
+print (dataHeart)
 
  
